46cc8cee9a2d8214fb03fffbf836f1aa4f27858e
Fahad Javed <jmuham@amazon.com>
2021-01-14 10:38:41 -0500

    Implement Create & Read Handlers
    * Update .rdpk-config and aws-lookoutvision-project.json schema definition
    
    * Implement Create/Read Handlers
    
    * Use injectCredentialsAndInvokeV2 in create and read handlers
    
    * Increase test coverage
    
    * Run mvn verify in CodeBuild
    
    * Remove unnecessary inheritance and deleted BaseHandlerStd abstract class
    
    * Update Delete, List, and Update to no-op

--- aws-lookoutvision-project/src/test/java/software/amazon/lookoutvision/project/CreateHandlerTest.java
+++ aws-lookoutvision-project/src/test/java/software/amazon/lookoutvision/project/CreateHandlerTest.java
@@ -1,13 +1,18 @@
 package software.amazon.lookoutvision.project;
 
-import java.time.Duration;
+import org.mockito.ArgumentMatchers;
 import software.amazon.awssdk.core.SdkClient;
+import software.amazon.awssdk.services.lookoutvision.model.CreateProjectResponse;
+import software.amazon.awssdk.services.lookoutvision.model.DescribeProjectResponse;
+import software.amazon.awssdk.services.lookoutvision.model.ProjectDescription;
+import software.amazon.awssdk.services.lookoutvision.model.ProjectMetadata;
+import software.amazon.awssdk.services.lookoutvision.model.ResourceNotFoundException;
+import software.amazon.cloudformation.exceptions.ResourceAlreadyExistsException;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.Logger;
 import software.amazon.cloudformation.proxy.OperationStatus;
 import software.amazon.cloudformation.proxy.ProgressEvent;
-import software.amazon.cloudformation.proxy.ProxyClient;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,54 +20,107 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class CreateHandlerTest extends AbstractTestBase {
+public class CreateHandlerTest {
+    private CreateHandler handler;
 
     @Mock
     private AmazonWebServicesClientProxy proxy;
 
     @Mock
-    private ProxyClient<SdkClient> proxyClient;
-
-    @Mock
-    SdkClient sdkClient;
+    private Logger logger;
 
     @BeforeEach
     public void setup() {
-        proxy = new AmazonWebServicesClientProxy(logger, MOCK_CREDENTIALS, () -> Duration.ofSeconds(600).toMillis());
-        sdkClient = mock(SdkClient.class);
-        proxyClient = MOCK_PROXY(proxy, sdkClient);
-    }
-
-    @AfterEach
-    public void tear_down() {
-        verify(sdkClient, atLeastOnce()).serviceName();
-        verifyNoMoreInteractions(sdkClient);
+        handler = new CreateHandler();
+        proxy = mock(AmazonWebServicesClientProxy.class);
+        logger = mock(Logger.class);
     }
 
     @Test
-    public void handleRequest_SimpleSuccess() {
-        final CreateHandler handler = new CreateHandler();
+    public void handleRequest_Success() {
+        final String projectName = "projectName";
+        final String projectArn = "arn:aws:lookoutvision:us-east-1:111111111111:project/projectName";
 
-        final ResourceModel model = ResourceModel.builder().build();
+        final ResourceNotFoundException firstDescribeResponse = ResourceNotFoundException.builder().build();
+        final CreateProjectResponse createProjectResponse = CreateProjectResponse.builder()
+            .projectMetadata(ProjectMetadata.builder()
+                .projectArn(projectArn)
+                .projectName(projectName)
+                .build())
+            .build();
+        final DescribeProjectResponse describeResponse = DescribeProjectResponse.builder()
+            .projectDescription(ProjectDescription.builder()
+                .projectName(projectName)
+                .projectArn(projectArn)
+                .build())
+            .build();
+
+        when(proxy.injectCredentialsAndInvokeV2(
+            ArgumentMatchers.any(),
+            ArgumentMatchers.any()
+        ))
+            .thenThrow(firstDescribeResponse)
+            .thenReturn(createProjectResponse)
+            .thenReturn(describeResponse);
+
+        final ResourceModel model = ResourceModel.builder()
+            .projectName(projectName)
+            .arn(projectArn)
+            .build();
 
         final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
             .desiredResourceState(model)
             .build();
 
-        final ProgressEvent<ResourceModel, CallbackContext> response = handler.handleRequest(proxy, request, new CallbackContext(), proxyClient, logger);
+        final ProgressEvent<ResourceModel, CallbackContext> response = handler.handleRequest(proxy, request, null, logger);
 
         assertThat(response).isNotNull();
         assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
+        assertThat(response.getCallbackContext()).isNull();
         assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
-        assertThat(response.getResourceModel()).isEqualTo(request.getDesiredResourceState());
         assertThat(response.getResourceModels()).isNull();
+        assertThat(response.getResourceModel()).isEqualToComparingFieldByField(model);
         assertThat(response.getMessage()).isNull();
         assertThat(response.getErrorCode()).isNull();
+    }
+
+    @Test
+    public void handleRequest_FailureAlreadyExists() {
+        final String projectName = "projectName";
+        final String projectArn = "arn:aws:lookoutvision:us-east-1:111111111111:project/projectName";
+        final ProjectDescription projectDescription = ProjectDescription.builder()
+            .projectName(projectName)
+            .projectArn(projectArn)
+            .build();
+
+        final DescribeProjectResponse describeResponseInitial = DescribeProjectResponse.builder()
+            .projectDescription(projectDescription)
+            .build();
+
+        doReturn(describeResponseInitial)
+            .when(proxy)
+            .injectCredentialsAndInvokeV2(
+                ArgumentMatchers.any(),
+                ArgumentMatchers.any()
+            );
+
+        final ResourceModel model = ResourceModel.builder()
+            .projectName(projectName)
+            .arn(projectArn)
+            .build();
+
+        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
+            .desiredResourceState(model)
+            .build();
+
+        assertThrows(ResourceAlreadyExistsException.class,
+            () -> handler.handleRequest(proxy, request, null, logger));
     }
 }
